56e4fc0bed3bbc00e46c13279298a2f3cf15d099
rugang.li <rugang.li@yingzt.com>
2017-04-22 17:46:05 +0800

    add file Signed-off-by: rugang.li <rugang.li@yingzt.com>

--- http/http_conn.h
+++ http/http_conn.h
@@ -84,25 +84,60 @@ public:
     bool write();
 private:
     void init();
-
+    /*解析HTTP请求*/
     HTTP_CODE process_read();
-
+    /*填充http response*/
     bool process_write(HTTP_CODE ret);
 
     HTTP_CODE parse_request_line(char* text);
-
     HTTP_CODE parse_headers(char* text);
-
     HTTP_CODE parse_content(char* text);
-
     HTTP_CODE do_request();
-
     char* get_line() { return read_buf_ + start_line_;}
-
     LINE_STATUS parse_line();
+    /*处理*/
+    void unmap();
+    bool add_response(const char * format, ...);
+    bool add_content(const char* content);
+    bool add_status_line(int status, const char* title);
+    bool add_content_length(int content_length);
+    bool add_headers(int header_lenght);
+    bool add_linger();
+    bool add_blank_line();
+
+public:
+    static int epollfd_;
+    static int user_count_;
+
+private:
+    int          sockfd_;
+    sockaddr_in  address_;
+
+    char    read_buf_[READ_BUFFER_SIZE];
+    int     read_idx_;
+    int     checked_idx;
+    int     start_line_;
+
+    char    write_buf_[WRITE_BUFFER_SIZE];
+    int     write_idx;
+
+    CHECK_STATE check_state_;
+    METHOD      method_;
+
+    /*请求文件路径, doc_root + url*/
+    char  read_file_[FILE_NAME_LEN];
     
-
-
+    char* url_;
+    /*协议版本号*/
+    char  version_;
+    char* host_;
+    int   content_length_;
+    /*是否保持连接*/
+    bool  linger_;
+    char* file_address_;
+    struct stat file_stat_;
+    struct iovec iv_[2]; 
+    int    iv_count_;
 };
 
 
